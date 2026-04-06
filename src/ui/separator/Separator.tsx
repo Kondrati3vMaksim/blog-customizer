6546526f59bba08c8cfc78a21f15052d3514853a
Maxim Kondratiev <kondrati3vmaksim@gmail.com>
2026-04-07 01:50:54 +0300

    version 1

--- src/ui/separator/Separator.tsx
+++ src/ui/separator/Separator.tsx
@@ -1,5 +1,19 @@
 import styles from './index.module.scss';
+import clsx from 'clsx';
 
-export const Separator = () => {
-	return <div className={styles.separator}></div>;
+type SeparatorProps = {
+	height?: number;
+	withLine?: boolean;
+};
+
+export const Separator = ({ height = 20, withLine = true }: SeparatorProps) => {
+	return (
+		<div
+			className={clsx({ [styles.separator]: withLine })}
+			style={{
+				height: `${height}px`,
+				background: withLine ? '#000000' : 'transparent',
+			}}
+		/>
+	);
 };
